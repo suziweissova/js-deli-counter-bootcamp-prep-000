16b33503470429c736fab1bd11108f93361b7631
Susann Weissova <suzi.weissova@gmail.com>
2018-06-25 09:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var katzDeli = []; 
-
 function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+function nowServing(){
+  
 }
